3c48f294bf8851d157c9acba0b72f462b93d7b2d
Himanga Piyumadi <hphewa2@gmail.com>
2026-03-13 23:03:11 +0530

    commit

--- tests/01-assertions.spec.js
+++ tests/01-assertions.spec.js
@@ -13,7 +13,7 @@ test('should load the app with correct title', async ({ page }) => {
   await expect(
     page.getByRole('heading', { name: 'Mini Task Manager' }) //checks visible heading on the page
   ).toBeVisible();
-  await page.pause();
+  
 });
 
 // 2. test case - It checks that the task list contains exactly 2 items.
@@ -24,7 +24,7 @@ test('should show default seeded tasks', async ({ page, taskPage }) => {
   await expect(taskPage.item('Learn Playwright')).toBeVisible(); //Checks that the task named Learn Playwright is visible on the page.
   
   await expect(taskPage.item('Write test cases')).toBeVisible(); //Checks that the task named Write test cases is visible on the page.
-  await page.pause();
+  
 
 });
 
@@ -37,7 +37,7 @@ test('should add a new task', async ({ page, taskPage }) => {
   await expect(taskPage.list).toHaveCount(3);
   
   await expect(taskPage.item('Prepare SEPQM demo')).toBeVisible();
-  await page.pause();
+  
 });
 
 // 4. test case - It marks a task as completed and checks that it is displayed as completed.
@@ -48,7 +48,7 @@ test('should mark a task as completed', async ({ page, taskPage }) => {
   await expect(
     taskPage.item('Learn Playwright').locator('.task-title')
   ).toHaveClass(/completed/); //This checks whether that task title element now has the CSS class completed
-  await page.pause();
+  
 });
 
 // 5. test case - It deletes a task and checks that it is removed from the list.
@@ -59,7 +59,7 @@ test('should delete a task', async ({ page, taskPage }) => {
   await expect(taskPage.item('Learn Playwright')).toHaveCount(0); //toHaveCount(0) verifies that zero matching elements exist.
   
   await expect(taskPage.list).toHaveCount(1);
-  await page.pause();
+  
 });
 
 // 6. test case - It tries to add a task without a title and checks that an error message is displayed and no new task is added.
@@ -70,6 +70,12 @@ test('should validate empty task input', async ({ page, taskPage }) => {
   await expect(page.getByTestId('message')).toHaveText('Task title is required');
   
   await expect(taskPage.list).toHaveCount(2);
-  await page.pause();
+  
+
+});
+
+// 7. test case
+test('demo fail - should incorrectly expect 3 seeded tasks', async ({ taskPage }) => {
+  await expect(taskPage.list).toHaveCount(3);
 
 });
