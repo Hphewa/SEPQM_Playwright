53f88d682163b6ee08fb7036075bc83e7f9a2776
Praneepa <praneepa871@gmail.com>
2026-03-13 10:45:39 +0530

    create 06-demo-status.spec.js for add one flaky test and one skipped test  3/13
    Flaky test:
      fails in the first run
      passes in retry
      report shows it as Flaky
    
    Skipped test:
      does not run
      report shows it as Skipped

--- tests/05-demo-fail.spec.js
+++ tests/05-demo-fail.spec.js
@@ -1,20 +1,5 @@
 const { test, expect } = require('./fixtures/base.fixture');
 
 test('demo fail - should incorrectly expect 3 seeded tasks', async ({ taskPage }) => {
-  await expect(taskPage.list).toHaveCount(3); // intentional fail
-});
-
-test('demo flaky - should pass on retry', async ({ taskPage }) => {
-  const retry = test.info().retry;
-
-  //a flaky test can be shown by making it fail on the first run and pass on retry
-  if (retry === 0) {
-    await expect(taskPage.list).toHaveCount(3); // fail on first run
-  }
-
-  await expect(taskPage.list).toHaveCount(2); // pass on retry
-});
-
-test.skip('demo skipped - should be skipped in report', async ({ taskPage }) => {
-  await expect(taskPage.list).toHaveCount(2); // skipped on purpose
+  await expect(taskPage.list).toHaveCount(3);
 });
---
+++ tests/06-demo-status.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('./fixtures/base.fixture');
+
+test('demo flaky - should pass on retry', async ({ taskPage }) => {
+  const retry = test.info().retry;
+
+  // fail on the first run to appear as flaky
+  if (retry === 0) {
+    await expect(taskPage.list).toHaveCount(3);
+  }
+
+  // pass on retry
+  await expect(taskPage.list).toHaveCount(2);
+});
+
+test.skip('demo skipped - should be skipped in report', async ({ taskPage }) => {
+  // skipped intentionally to appear in report
+  await expect(taskPage.list).toHaveCount(2);
+});
